abddb0a2583f22fe636ebc9817180a18fa354af2
Avinash <palakurthiavinash07@gmail.com>
2024-06-17 11:58:24 +0530

    project setup & pages created

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/signin" element={<Signin />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/dashboard" element={<Dashboard />} />
+				<Route path="/projects" element={<Projects />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
